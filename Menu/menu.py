65dfbc7fc9be6478f58584e759f2b6f7a820059c
Arslonov Sunnatjon <118212317+MohirFest22@users.noreply.github.com>
2023-07-30 23:11:54 +0500

    Finally. program ready

---
+++ Menu/menu.py
@@ -0,0 +1,33 @@
+from tkinter import *
+from tkinter.filedialog import *
+
+tk = Tk()
+tk.title("Menu")
+tk.geometry("800x900")
+
+def openfile():
+	faylname = askopenfilename()
+def newfile():
+	pass
+def savefile():
+	faylname = asksaveasfilename()
+Menyu = Menu()
+tk.config(menu=Menyu)
+
+fayl = Menu(Menyu,tearoff=0)
+fayl.add_cascade(label="Open file",command=openfile)
+fayl.add_cascade(label="New file",command=newfile)
+fayl.add_cascade(label="Save",command=savefile)
+fayl.add_separator()
+fayl.add_cascade(label="Exit",command=tk.destroy)
+
+edit = Menu(Menyu,tearoff=0)
+edit.add_cascade(label="Cut")
+edit.add_cascade(label="Copy")
+edit.add_cascade(label="Past")
+
+
+Menyu.add_cascade(label="File",menu=fayl)
+Menyu.add_cascade(label="Edit",menu=edit)
+
+tk.mainloop()
